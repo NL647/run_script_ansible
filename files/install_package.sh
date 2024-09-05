f1512c2b42b798e897991baf65e671a7aad2f441
NL647 <76846739+NL647@users.noreply.github.com>
2024-09-05 07:19:58 +0200

    Create install_package.sh

---
+++ files/install_package.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+echo -e "================================="
+echo -e "Installing unattended-upgrades..."
+echo -e "================================="
+lsb_relase -a || >&2
+
+sudo apt update
+sudo apt-get install unattended-upgrades -y
+sudo echo "unattended-upgrades       unattended-upgrades/enable_auto_updates boolean true" | sudo debconf-set-selections && sudo dpkg-reconfigure -f noninteractive unattended-upgrades || echo -e "Error installing package...."
+
+echo -e "================================="
+echo -e "DONE..."
+echo -e "================================="
+
+exit 0
